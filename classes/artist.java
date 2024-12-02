7c468bd01d62bc071531c31eda86d4d7d885d923
BarisTerbillioglu <terbillioglubaris@gmail.com>
2024-12-03 00:42:57 +0300

    artist and song class variables and getters

--- classes/artist.java
+++ classes/artist.java
@@ -1,6 +1,23 @@
 package classes;
 
+import java.util.ArrayList;
+
+
 public class artist {
 
+    private ArrayList<song> popularSongs;
+    private String ARTISTNAME;
+    private int monthlyListener;
     
+    public ArrayList<song> getPopularSongs(){
+        return popularSongs;
+    }
+
+    public String getARTISTNAME() {
+        return ARTISTNAME;
+    }
+
+    public int getMonthlyListener() {
+        return monthlyListener;
+    }
 }
