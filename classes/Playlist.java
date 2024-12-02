b3d1c487adb43f81474746e01ab41ba5258541bf
Gürkan Çakmak <gurkancakmakk@hotmail.com>
2024-12-03 00:46:54 +0300

    playlist and profile object classes added

---
+++ classes/Playlist.java
@@ -0,0 +1,86 @@
+package classes;
+
+import java.time.LocalDateTime;
+
+import javax.swing.ImageIcon;
+
+public class Playlist {
+
+    //variables
+    protected int totalTime;
+    protected boolean isGenre;
+    protected String playlistName;
+    protected ImageIcon pictureOfPlaylist;
+    protected LocalDateTime date;
+    protected int like;
+    protected int songSize;
+    protected int mainMood;
+    protected String mainGenre;
+    protected profile owner;
+
+    //getters and setters
+    public int getTotalTime() {
+        return totalTime;
+    }
+    public void setTotalTime(int totalTime) {
+        this.totalTime = totalTime;
+    }
+    public boolean isGenre() {
+        return isGenre;
+    }
+    public void setGenre(boolean isGenre) {
+        this.isGenre = isGenre;
+    }
+    public String getPlaylistName() {
+        return playlistName;
+    }
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+    public ImageIcon getPictureOfPlaylist() {
+        return pictureOfPlaylist;
+    }
+    public void setPictureOfPlaylist(ImageIcon pictureOfPlaylist) {
+        this.pictureOfPlaylist = pictureOfPlaylist;
+    }
+    public LocalDateTime getDate() {
+        return date;
+    }
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+    public int getLike() {
+        return like;
+    }
+    public void setLike(int like) {
+        this.like = like;
+    }
+    public int getSongSize() {
+        return songSize;
+    }
+    public void setSongSize(int songSize) {
+        this.songSize = songSize;
+    }
+    public int getMainMood() {
+        return mainMood;
+    }
+    public void setMainMood(int mainMood) {
+        this.mainMood = mainMood;
+    }
+    public String getMainGenre() {
+        return mainGenre;
+    }
+    public void setMainGenre(String mainGenre) {
+        this.mainGenre = mainGenre;
+    }
+    public profile getOwner() {
+        return owner;
+    }
+    public void setOwner(profile owner) {
+        this.owner = owner;
+    }
+
+
+
+
+}
--- classes/profile.java
+++ classes/profile.java
@@ -1,5 +1,84 @@
 package classes;
 
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+
 public class profile {
-    
+
+    // variables - features of profiles
+    protected ArrayList <profile> followers;
+    protected ArrayList <profile> followings;
+    protected ArrayList <Playlist> playlists;
+    protected ImageIcon profilePicture;
+    protected int currentMood;
+    protected String favoriteGenre;
+    protected String password;
+    protected String userName;
+    protected String mail;
+
+
+
+
+
+
+
+    //getters and setters
+    public ArrayList<profile> getFollowers() {
+        return followers;
+    }
+    public void setFollowers(ArrayList<profile> followers) {
+        this.followers = followers;
+    }
+    public ArrayList<profile> getFollowings() {
+        return followings;
+    }
+    public void setFollowings(ArrayList<profile> followings) {
+        this.followings = followings;
+    }
+    public ArrayList<Playlist> getPlaylists() {
+        return playlists;
+    }
+    public void setPlaylists(ArrayList<Playlist> playlists) {
+        this.playlists = playlists;
+    }
+    public ImageIcon getProfilePicture() {
+        return profilePicture;
+    }
+    public void setProfilePicture(ImageIcon profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+    public int getCurrentMood() {
+        return currentMood;
+    }
+    public void setCurrentMood(int currentMood) {
+        this.currentMood = currentMood;
+    }
+    public String getFavoriteGenre() {
+        return favoriteGenre;
+    }
+    public void setFavoriteGenre(String favoriteGenre) {
+        this.favoriteGenre = favoriteGenre;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+
+
+
 }
